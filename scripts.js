7a6ea7d978b1243b026e9a2f9d68f2f88e6b298f
Rhen <2021-102331@rtu.edu.ph>
2024-09-13 15:05:50 +0800

    changes

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
 /* Place your JavaScript in this file */
+
+function sortList(ul) {
+    var ul = document.getElementById(ul);
+  
+    Array.from(ul.getElementsByTagName("LI"))
+      .sort((a, b) => a.textContent.localeCompare(b.textContent))
+      .forEach(li => ul.appendChild(li));
+  }
+  
+  sortList("branches");
