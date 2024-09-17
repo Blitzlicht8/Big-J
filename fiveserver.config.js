df391662159cbe9c6876c0bbe7b2a286d085c513
Rhen <2021-102331@rtu.edu.ph>
2024-09-17 23:30:29 +0800

    php changes

---
+++ fiveserver.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    php: 'D:\\Programming\\xampp\\php\\php.exe' // Windows (example)
+  }
--- forms/contact.php
+++ forms/contact.php
@@ -1,13 +1,59 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+ 
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+ 
+//required files
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+ 
+//Create an instance; passing `true` enables exceptions
+if (isset($_POST["submitContact"])) {
+ 
+  $mail = new PHPMailer(true);
+ 
+    //Server settings
+    $mail->isSMTP();                              //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';       //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;             //Enable SMTP authentication
+    $mail->Username   = 'rhenallenpabalan@gmail.com';   //SMTP write your email
+    $mail->Password   = 'xhlyjobhrkkoovzs';      //SMTP password
+    $mail->SMTPSecure = 'ssl';            //Enable implicit SSL encryption
+    $mail->Port       = 465;                                    
+ 
+    //Recipients
+    $mail->setFrom( $_POST["email"], $_POST["name"]); // Sender Email and name
+    $mail->addAddress('rhenallenpabalan@gmail.com');     //Add a recipient email  
+    $mail->addReplyTo($_POST["email"], $_POST["name"]); // reply to sender email
+ 
+    //Content
+    $mail->isHTML(true);               //Set email format to HTML
+    $mail->Subject = $_POST["subject"];   // email subject headings
+    $mail->Body = "Email: {$_POST["email"]}\n\nName: {$_POST["name"]}\n\nMessage: {$_POST["message"]}";   //email body (email/name/message)
+    
+      
+    // Validation and Success/Failure sent message alert
+    $mail->send();
+    if ($mail->send()) {
+      echo 
+      "
+      <script> 
+     alert('Message was sent successfully!');
+     document.location.href = 'index.html#contact';
+    </script>
+    "
+      ;
+    } else {
+      echo 'Email could not be sent. Error: ' . $mail->ErrorInfo;
+    }
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'rhenallenpabalan@gmail.com';
+}
+
+  
+  /*$receiving_email_address = 'rhenallenpabalan@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -23,19 +69,18 @@
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
     'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'username' => 'PHPMailer',
+    'password' => 'xhlyjobhrkkoovzs',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
+  echo $contact->send();*/
 ?>
