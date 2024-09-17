ffaa70af6661fec44e90517b5328446c71061636
Rhen <2021-102331@rtu.edu.ph>
2024-09-17 18:50:14 +0800

    changes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -28,7 +28,7 @@ lastScrollTop = scrollTop;
 });
 
 // Get the button:
-let mybutton = document.getElementById("myBtn");
+let mybutton = document.getElementById("goUp");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
