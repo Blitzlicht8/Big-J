f352bef863f8d38a899647886205a47f1271a2eb
Rhen <2021-102331@rtu.edu.ph>
2024-10-30 13:23:16 +0800

    forms

--- getinvolved.php
+++ getinvolved.php
@@ -13,6 +13,10 @@
   <div class="container">
   <iframe src="https://outlook.office365.com/owa/calendar/9a127b550d57423c82ebcdd2903528bd@globalsurge.org/dc93431b4d3e41cca3958b83a454d56410072994079746236359/calendar.html" scrolling="no" width="100%" height="700" frameborder="0"></iframe>
   </div>
+  <hr class="hrstyle">
+  <div class="container">
+  <iframe width="50%" height= "750px" src= "https://forms.office.com/Pages/ResponsePage.aspx?id=y6x0CZKnxkCEUotRekCR43XChLSdMUVIvtvSe4ObjdFUQVlSMUFFUllGS1BWMkhJRjQ1UTRKVTRCQy4u&embed=true"> </iframe>
+  </div>
   
   <!-- End of Main -->
   <hr class="hrstyle">
